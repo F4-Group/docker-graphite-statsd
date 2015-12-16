3591cc86e47a6f99ab3beeb7f3bfda86cbad38be
Ludovic Perrine <lpe@f4-group.com>
2015-12-16 16:47:06 +0100

    Mark migration as done if no database exists (fresh install)

--- scripts/migrate_db_1_4.sh
+++ scripts/migrate_db_1_4.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -e
 
-if [ ! -f /opt/graphite/storage/graphite_db_migrated_to_1.4 ]; then
-    cp /opt/graphite/storage/graphite.db /opt/graphite/storage/graphite.db.backup-`date +%Y%m%d_%H%M%S`
-    PYTHONPATH=/opt/graphite/webapp django-admin.py syncdb --noinput --no-initial-data --settings=graphite.settings
-    touch /opt/graphite/storage/graphite_db_migrated_to_1.4
+stampfile=/opt/graphite/storage/graphite_db_migrated_to_1.4
+
+if [ ! -f $stampfile ]; then
+    if [ -f /opt/graphite/storage/graphite.db ]; then
+        cp /opt/graphite/storage/graphite.db /opt/graphite/storage/graphite.db.backup-`date +%Y%m%d_%H%M%S`
+        PYTHONPATH=/opt/graphite/webapp django-admin.py syncdb --noinput --no-initial-data --settings=graphite.settings
+    fi
+    touch $stampfile
 fi
